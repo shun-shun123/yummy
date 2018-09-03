f8f05b3c80280f8e1ffaa90aecb8cc0f55516119
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-09-04 03:36:09 +0900

    :lipstick:CameraActivityにもBottomNavigationViewを追加

---
+++ app/src/main/java/shunsuke/yummy/watnow/jp/yummy/Camera/CameraActivity.kt
@@ -0,0 +1,12 @@
+package shunsuke.yummy.watnow.jp.yummy.Camera
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CameraActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
