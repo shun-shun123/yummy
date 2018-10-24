4678447856436b8587e519510fd7e0dd07c6a1cb
Shunsuke Saito <shunsuke15vb@gmail.com>
2018-10-24 17:33:34 +0900

    :sparkles:レイアウトを大幅修正Activityを3つにして変更

--- app/src/main/java/shunsuke/yummy/watnow/jp/yummy/Home/ConfigurationActivity.kt
+++ app/src/main/java/shunsuke/yummy/watnow/jp/yummy/Home/ConfigurationActivity.kt
@@ -4,26 +4,23 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_configuration.*
 import shunsuke.yummy.watnow.jp.yummy.R
 
-val REQUEST_IMAGE_CAPTURE: Int = 1
+class ConfigurationActivity : AppCompatActivity() {
 
-class HomeActivity : AppCompatActivity() {
-
-    private val mContext: Context = this@HomeActivity
-    private val MENU_ID: Int = 0
-
+    private val mContext: Context = this@ConfigurationActivity
+    private val MENU_ID: Int = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_configuration)
 
         bottomNavigationView.menu.getItem(MENU_ID).isChecked = true
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             val intent: Intent? = when (item.itemId) {
-                bottomNavigationView.menu.getItem(1).itemId -> Intent(this@HomeActivity, SearchActivity::class.java)
-                bottomNavigationView.menu.getItem(2).itemId -> Intent(this@HomeActivity, ConfigurationActivity::class.java)
+                bottomNavigationView.menu.getItem(0).itemId -> Intent(this@ConfigurationActivity, HomeActivity::class.java)
+                bottomNavigationView.menu.getItem(1).itemId -> Intent(this@ConfigurationActivity, SearchActivity::class.java)
                 else -> null
             }
             if (intent != null) {
--- app/src/main/java/shunsuke/yummy/watnow/jp/yummy/Home/SearchActivity.kt
+++ app/src/main/java/shunsuke/yummy/watnow/jp/yummy/Home/SearchActivity.kt
@@ -4,26 +4,23 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.activity_search.*
 import shunsuke.yummy.watnow.jp.yummy.R
 
-val REQUEST_IMAGE_CAPTURE: Int = 1
+class SearchActivity : AppCompatActivity() {
 
-class HomeActivity : AppCompatActivity() {
-
-    private val mContext: Context = this@HomeActivity
-    private val MENU_ID: Int = 0
-
+    private val mContext: Context = this@SearchActivity
+    private val MENU_ID: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_search)
 
         bottomNavigationView.menu.getItem(MENU_ID).isChecked = true
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             val intent: Intent? = when (item.itemId) {
-                bottomNavigationView.menu.getItem(1).itemId -> Intent(this@HomeActivity, SearchActivity::class.java)
-                bottomNavigationView.menu.getItem(2).itemId -> Intent(this@HomeActivity, ConfigurationActivity::class.java)
+                bottomNavigationView.menu.getItem(0).itemId -> Intent(this@SearchActivity, HomeActivity::class.java)
+                bottomNavigationView.menu.getItem(2).itemId -> Intent(this@SearchActivity, ConfigurationActivity::class.java)
                 else -> null
             }
             if (intent != null) {
